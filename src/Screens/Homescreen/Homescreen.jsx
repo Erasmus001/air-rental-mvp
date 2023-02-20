ad12d7a31f7c7872a4de56c82acc217898c5424f
Erasmus001 <erasmusmensah00@gmail.com>
2023-02-20 06:44:55 +0000

    minor commit

--- src/Screens/Homescreen/Homescreen.jsx
+++ src/Screens/Homescreen/Homescreen.jsx
@@ -2,9 +2,23 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
 const Homescreen = () => {
-	return <View style={styles.homescreen}></View>;
+	return (
+		<View style={styles.homescreen}>
+			<Text style={styles.title}>Home Screen</Text>
+		</View>
+	);
 };
 
 export default Homescreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	homescreen: {
+		flex: 1,
+		height: '100vh',
+		backgroundColor: '#eee',
+		textAlign: 'center',
+	},
+	title: {
+		fontSize: 30,
+	},
+});
